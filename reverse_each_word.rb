dd88d51d7416a4d2f6202505fc89e695d7f11d77
Dennis Lord <lorddennis1006@gmail.com>
2018-05-16 17:36:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
-  sentence1 = "Hello, there an d who are you?"
+  sentence1 == "Hello, there an d who are you?"
   words = sentence1.split(' ')
   result = []
   (words.length - 1).downto(0) do |i|
