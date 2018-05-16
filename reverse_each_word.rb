6f98340fc494aa464194c8802dad0a0574c514a1
Dennis Lord <lorddennis1006@gmail.com>
2018-05-16 17:32:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
-def reverse_each_word(sentence1)
-  sentence1.reverse "Hello, there and who are you?"
+reverse_each_word = "Hello, there and who are you?"
+  reverse_each_word.reverse "Hello, there and who are you?"
 end
